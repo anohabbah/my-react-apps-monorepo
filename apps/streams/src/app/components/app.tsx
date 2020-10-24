d9e5376c658e51b4521881bae80ceaec62a2740d
Abbah <anohabbah@gmail.com>
2020-10-25 00:18:58 +0200

    Clean project

---
+++ apps/streams/src/app/components/app.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { Route, Link } from 'react-router-dom';
+
+export const App = () => {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./app.scss file.
+   */
+  return (
+    <div className="app">
+      <Route
+        path="/"
+        exact
+        render={() => (
+          <div>
+            This is the generated root route.{' '}
+            <Link to="/page-2">Click here for page 2.</Link>
+          </div>
+        )}
+      />
+      <Route
+        path="/page-2"
+        exact
+        render={() => (
+          <div>
+            <Link to="/">Click here to go back to root page.</Link>
+          </div>
+        )}
+      />
+      {/* END: routes */}
+    </div>
+  );
+};
+
+export default App;
