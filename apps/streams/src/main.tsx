9b8081ee2954351d64b789774180a1f87e838c96
Abbah <anohabbah@gmail.com>
2020-10-25 12:56:19 +0100

    feat(form-state): setup a form state
    Using redux-form package

--- apps/streams/src/main.tsx
+++ apps/streams/src/main.tsx
@@ -6,7 +6,10 @@ import {  createStore } from 'redux';
 import App from './app/components/app';
 import reducers from './app/core/reducers'
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  window['__REDUX_DEVTOOLS_EXTENSION__'] && window['__REDUX_DEVTOOLS_EXTENSION__']()
+);
 
 ReactDOM.render(
   <React.StrictMode>
