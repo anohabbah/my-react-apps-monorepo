63ad832454e50983014ab8c8369ed35321264bb4
Abbah <anohabbah@gmail.com>
2020-10-25 17:12:51 +0100

    feat(api): end up json server api

--- apps/json-api/src/main.ts
+++ apps/json-api/src/main.ts
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+import { join } from 'path';
+import jsonServer from 'json-server';
+import debug from 'debug';
+
+// eslint-disable-next-line
+require('dotenv').config();
+
+const logger = debug('json:api');
+
+const server = jsonServer.create();
+const router = jsonServer.router(join(__dirname, 'assets', 'db.json'));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+server.listen(4300, () => {
+  logger('APP is running');
+})
