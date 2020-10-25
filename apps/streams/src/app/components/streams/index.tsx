b77f90b2ad0e84213614f2ccd1fd530af216d99c
Abbah <anohabbah@gmail.com>
2020-10-25 20:43:39 +0100

    feat(streams): render Streams list

--- apps/streams/src/app/components/streams/index.tsx
+++ apps/streams/src/app/components/streams/index.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-/* eslint-disable-next-line */
-export interface IndexProps {}
+import { fetchStreams } from '../../core/actions';
 
-export const Index = (props: IndexProps) => {
-  return (
-    <div>Streams List Page</div>
-  );
-};
+export interface IndexProps {
+  streams: { id, title, description }[];
+  fetchStreams;
+}
 
-export default Index;
+export class Index extends React.Component<IndexProps> {
+  renderList = () => {
+    return this.props.streams.map(stream => (
+      <div className="item" key={stream.id}>
+        <i className="large middle aligned icon camera" />
+        <div className="content">
+          {stream.title}
+          <div className="description">{stream.description}</div>
+        </div>
+      </div>
+    ));
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({ streams: Object.values(state.streams) });
+
+export default connect(
+  mapStateToProps,
+  { fetchStreams }
+)(Index);
