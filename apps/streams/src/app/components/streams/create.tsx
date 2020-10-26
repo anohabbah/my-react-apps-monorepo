dbc2a41b30f56d874c01b12eb05f20b4ff6dd464
Abbah <anohabbah@gmail.com>
2020-10-26 16:24:47 +0100

    Reuse Stream form component

--- apps/streams/src/app/components/streams/create.tsx
+++ apps/streams/src/app/components/streams/create.tsx
@@ -1,76 +1,31 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
+
 import { createStream } from '../../core/actions';
+import StreamForm from '../../core/components/stream-form';
 
 export interface CreateProps {
   createStream;
 }
 
 export class StreamCreate extends React.Component<CreateProps> {
-  renderError = ({ error, touched }) => {
-    if (touched && error) {
-      return <div className="ui error message">
-        <div className="header">{error}</div>
-      </div>;
-    }
-
-    return null;
-  };
-
-  renderInput = ({ input, label, meta }) => (
-    <div className={`field ${meta.error && meta.touched ? 'error' : ''} `}>
-      <label>{label}</label>
-      <input {...input} />
-      {this.renderError(meta)}
-    </div>
-  );
-
   onSubmit = ({ title, description }) => {
     this.props.createStream({ title, description })
   };
 
   render() {
     return (
-      <form
-        className="ui form error"
-        onSubmit={this.props['handleSubmit'](this.onSubmit)}
-      >
-        <Field component={this.renderInput} name="title" label="Enter Title" />
-        <Field
-          component={this.renderInput}
-          name="description"
-          label="Enter Description"
-        />
-
-        <button className="ui button primary" type="submit">Create</button>
-      </form>
+      <div>
+        <h3>Create a stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
-
-const validate = ({ description, title }) => {
-  const errors = {};
-
-  if (!title) {
-    errors['title'] = 'Title field is required.';
-  }
-
-  if (!description) {
-    errors['description'] = 'Description field is required.';
-  }
-
-  return errors;
-};
-
-const formWrapped = reduxForm({
-  form: 'streamCreate',
-  validate
-})(StreamCreate);
 
 export default connect(
   null,
   {
     createStream
   }
-)(formWrapped);
+)(StreamCreate);
--- apps/streams/src/app/components/streams/edit.tsx
+++ apps/streams/src/app/components/streams/edit.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import { connect, MapStateToProps } from 'react-redux';
+import pick from 'lodash/pick';
 
 import { RootState } from '../../core/reducers';
-import { fetchStream } from '../../core/actions';
+import { fetchStream, editStream } from '../../core/actions';
+import StreamForm from '../../core/components/stream-form';
 
 export interface EditProps {
   stream: { id: string, title: string, description: string, } | undefined;
-  fetchStream: (id:  string) => void;
+  fetchStream: (id: string) => Promise<void>;
+  editStream: (id: string, data: {title: string, description: string}) => Promise<void>;
   match;
 }
 
 export class StreamEdit extends React.Component<EditProps> {
   componentDidMount() {
-    this.props.fetchStream(this.props.match.param.streamId);
+    this.props.fetchStream(this.props.match.params.streamId);
+  }
+
+  onSubmit = (values) => {
+    this.props.editStream(this.props.stream.id, values);
   }
 
   render() {
+    if (!this.props.stream) return <span>Loading...</span>;
+
     return (
       <div>
-        <h1>Welcome to edit a stream page!</h1>
+        <h3>Edit a stream</h3>
+        <StreamForm
+          onSubmit={this.onSubmit}
+          initialValues={pick(this.props.stream, 'title', 'description')}
+        />
       </div>
     );
   }
@@ -26,12 +39,13 @@ export class StreamEdit extends React.Component<EditProps> {
 
 const mapStateToProps: MapStateToProps<Record<string, unknown>, Record<string, unknown>, RootState> =
   (state, ownProps) => {
-  return { stream: state.streams[ownProps.match['param'].streamId], };
+  return { stream: state.streams[ownProps.match['params'].streamId], };
 };
 
 export default connect(
   mapStateToProps,
   {
     fetchStream,
+    editStream
   }
 )(StreamEdit);
