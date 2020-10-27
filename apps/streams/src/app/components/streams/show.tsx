bf37f08376193fcb50424544691e70f3a953727f
Abbah <anohabbah@gmail.com>
2020-10-27 17:28:48 +0100

    feat(stream-player): add a video player to /streams/streamId page to player streams

--- apps/streams/src/app/components/streams/show.tsx
+++ apps/streams/src/app/components/streams/show.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { RefObject } from 'react';
 import { connect, MapStateToProps } from 'react-redux';
+import Flv from 'flv.js';
+
 import { RootState } from '../../core/reducers';
 import { fetchStream } from '../../core/actions';
 
@@ -10,9 +12,39 @@ export interface ShowProps {
 }
 
 export class StreamShow extends React.Component<ShowProps> {
+  videoRef: RefObject<HTMLVideoElement>;
+  flvPlayer: Flv.Player
+  constructor(props: ShowProps) {
+    super(props);
+
+    this.videoRef = React.createRef<HTMLVideoElement>();
+  }
+
   componentDidMount() {
     const streamId: string = this.props.match.params.streamId;
+
     this.props.fetchStream(streamId);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate(
+    prevProps: Readonly<ShowProps>,
+    prevState: Readonly<unknown>,
+    snapshot?: unknown
+  ) {
+    this.buildPlayer();
+  }
+
+  buildPlayer = () => {
+    if (this.flvPlayer || !this.props.stream) return;
+
+    this.flvPlayer = Flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:4400/live/${this.props.stream.id}.flv`
+    });
+    this.flvPlayer.attachMediaElement(this.videoRef.current);
+    this.flvPlayer.load();
+    // this.flvPlayer.play();
   }
 
   render() {
@@ -23,6 +55,11 @@ export class StreamShow extends React.Component<ShowProps> {
     const { description, title } = this.props.stream;
     return (
       <div>
+        <video
+          ref={this.videoRef}
+          style={{ width: '100%' }}
+          controls
+        />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
