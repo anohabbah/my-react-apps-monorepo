9fe839ca72bfe9e7504c01856b43b6a2d6223289
Abbah <anohabbah@gmail.com>
2020-10-27 16:17:47 +0100

    feat(rtmp): setup a node media server

--- apps/rtmp-server/src/main.ts
+++ apps/rtmp-server/src/main.ts
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+import { NodeMediaServer } from 'node-media-server';
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 4400,
+    allow_origin: '*'
+  }
+};
+
+const nms = new NodeMediaServer(config);
+nms.run();
