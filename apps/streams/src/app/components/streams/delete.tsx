bc3e193329dcae84ec5981dfd59678464cfd4f09
Abbah <anohabbah@gmail.com>
2020-10-27 11:51:03 +0100

    Add a modal to delete stream page

--- apps/streams/src/app/components/streams/delete.tsx
+++ apps/streams/src/app/components/streams/delete.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Modal from '../../core/components/modal';
+
 /* eslint-disable-next-line */
 export interface DeleteProps {}
 
@@ -7,6 +9,7 @@ export const StreamDelete = (props: DeleteProps) => {
   return (
     <div>
       <h1>Welcome to delete a stream page!</h1>
+      <Modal />
     </div>
   );
 };
